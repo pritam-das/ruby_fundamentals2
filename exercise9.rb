7598a699ec9a2995a8e083b7b88c30cd408e465a
pritam-das <pritam.das@mail.utoronto.ca>
2017-03-28 15:47:06 -0400

    grocery list exercise step 1.1 done

--- exercise9.rb
+++ exercise9.rb
@@ -1,6 +1,19 @@
+def grocery_list_additions(item)
+  puts "what item do you want to add?"
+  response = gets.chomp
+  item << response
+  return item
+end
+
 grocery_list = ["carrots", "toilet papers", "apples", "salmon"]
 
 grocery_list.each do |gl|
       puts "* #{gl}\n"
 
-end      
+end
+updated_grocery_list=grocery_list_additions(grocery_list)
+
+updated_grocery_list.each do |gl|
+      puts "* #{gl}\n"
+
+end
